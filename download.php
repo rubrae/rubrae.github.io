15f39705cb71758c4110f6879d1ee83a28d76e64
Rubrae <60319734+rubrae@users.noreply.github.com>
2025-07-10 06:54:37 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,11 +1,25 @@
 <?php
 if (isset($_GET['link'])) {
-  $url = $_GET['link'];
-  $basename = basename($url);
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header("Content-Disposition: attachment; filename=\"$basename\"");
-  readfile($url);
-  exit;
+    $url = $_GET['link'];
+
+    // Vérifie SSL
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        http_response_code(400);
+        exit("URL invalide.");
+    }
+
+    $filename = basename(parse_url($url, PHP_URL_PATH));
+
+    // Récupère 
+    $imageData = @file_get_contents($url);
+    if ($imageData === false) {
+        http_response_code(404);
+        exit("Fichier introuvable.");
+    }
+    header('Content-Type: application/octet-stream');
+    header("Content-Disposition: attachment; filename=\"$filename\"");
+    header('Content-Length: ' . strlen($imageData));
+    echo $imageData;
+    exit;
 }
 ?>
