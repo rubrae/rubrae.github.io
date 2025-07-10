dcf537ca6bc54cd59b9042b26cf8cae1e324783f
Rubrae <60319734+rubrae@users.noreply.github.com>
2025-07-10 06:58:51 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,25 +1,38 @@
 <?php
-if (isset($_GET['link'])) {
-    $url = $_GET['link'];
-
-    // Vérifie SSL
-    if (!filter_var($url, FILTER_VALIDATE_URL)) {
-        http_response_code(400);
-        exit("URL invalide.");
-    }
-
-    $filename = basename(parse_url($url, PHP_URL_PATH));
-
-    // Récupère 
-    $imageData = @file_get_contents($url);
-    if ($imageData === false) {
-        http_response_code(404);
-        exit("Fichier introuvable.");
-    }
-    header('Content-Type: application/octet-stream');
-    header("Content-Disposition: attachment; filename=\"$filename\"");
-    header('Content-Length: ' . strlen($imageData));
-    echo $imageData;
-    exit;
+if (!isset($_GET['link'])) {
+    http_response_code(400);
+    exit("Lien manquant.");
 }
+
+$url = $_GET['link'];
+
+if (!filter_var($url, FILTER_VALIDATE_URL)) {
+    http_response_code(400);
+    exit("Lien invalide.");
+}
+
+$filename = basename(parse_url($url, PHP_URL_PATH));
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$data = curl_exec($ch);
+$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+curl_close($ch);
+
+if ($data === false) {
+    http_response_code(500);
+    exit("Erreur lors du téléchargement.");
+}
+
+header('Content-Description: File Transfer');
+header('Content-Type: ' . $contentType);
+header('Content-Disposition: attachment; filename="' . $filename . '"');
+header('Content-Length: ' . strlen($data));
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Expires: 0');
+
+echo $data;
+exit;
 ?>
