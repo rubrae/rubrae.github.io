2561b71c04869977da28fc0e6e9db485a4a15e64
Rubrae <60319734+rubrae@users.noreply.github.com>
2025-07-10 07:26:58 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,50 +1,51 @@
 <?php
-echo "Script PHP fonctionne !<br>";
-echo "Paramètres GET reçus :<br>";
-var_dump($_GET);
-
-if (isset($_GET['link'])) {
-    echo "<br>URL à télécharger : " . $_GET['link'] . "<br>";
-    
-    $url = urldecode($_GET['link']);
-    echo "URL décodée : " . $url . "<br>";
-    
-    if (filter_var($url, FILTER_VALIDATE_URL)) {
-        echo "URL valide ✓<br>";
-    } else {
-        echo "URL invalide ✗<br>";
-    }
-    
-    if (function_exists('curl_init')) {
-        echo "cURL disponible ✓<br>";
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_NOBODY, true); 
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
-        $result = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-        
-        echo "Code HTTP : " . $httpCode . "<br>";
-        echo "Type de contenu : " . $contentType . "<br>";
-        
-        if (curl_errno($ch)) {
-            echo "Erreur cURL : " . curl_error($ch) . "<br>";
-        }
-        
-        curl_close($ch);
-    } else {
-        echo "cURL non disponible ✗<br>";
-    }
+if (!isset($_GET['link'])) {
+    http_response_code(400);
+    die('Paramètre link manquant');
 }
-?>
 
-<form method="GET">
-    <label>Testez avec une URL :</label><br>
-    <input type="text" name="link" value="https://httpbin.org/image/png" style="width: 400px;"><br>
-    <input type="submit" value="Tester">
-</form>
+$url = trim($_GET['link']);
+
+if (!filter_var($url, FILTER_VALIDATE_URL)) {
+    http_response_code(400);
+    die('URL invalide');
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36');
+
+$data = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+if (curl_errno($ch)) {
+    curl_close($ch);
+    http_response_code(500);
+    die('Erreur de téléchargement');
+}
+
+curl_close($ch);
+
+if ($httpCode !== 200 || !$data) {
+    http_response_code(404);
+    die('Fichier introuvable');
+}
+
+$filename = basename(parse_url($url, PHP_URL_PATH));
+if (empty($filename)) {
+    $filename = 'download';
+}
+
+ob_clean();
+header('Content-Type: ' . ($contentType ?: 'application/octet-stream'));
+header('Content-Disposition: attachment; filename="' . $filename . '"');
+header('Content-Length: ' . strlen($data));
+
+echo $data;
+exit;
+?>
